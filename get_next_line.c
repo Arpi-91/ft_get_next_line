354eb679400df1701b9b76f96a90501346650c0c
Arpine Arakelyan <ararakel@c1r3s3.42yerevan.am>
2025-04-08 18:39:20 +0400

    added commet 3

--- get_next_line.c
+++ get_next_line.c
@@ -6,90 +6,59 @@
 /*   By: ararakel <ararakel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/05 16:03:01 by ararakel          #+#    #+#             */
-/*   Updated: 2025/04/07 20:02:26 by ararakel         ###   ########.fr       */
+/*   Updated: 2025/04/08 17:28:01 by ararakel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-// char *fill_temp_line()
-// {
-//     static char* tempft_strdup_line;
 
-    
-    
-// }
-
-char *set_line(char * temp_line)
-{
+char *set_line(char *temp_line) {
     char *line;
-    int i;
-    int len;
     
-    i = 0;
-    
-    while(temp_line[i])
-    {
-        if(temp_line[i] == '\n')
-        {
-            break;
-        }
+    int i = 0;
+    while (temp_line[i] && temp_line[i] != '\n') {
         i++;
     }
-    printf("%d\n", i);
-    len = ft_strlen(temp_line) - i;
-
-
-    line = ft_substr(temp_line, 0, len);
-    printf("gnl ---->%s->", line);
+    line = ft_substr(temp_line, 0, i + 1);
     return (line);
 }
 
-char *get_next_line(int fd)
-{
+char *get_next_line(int fd) {
     char *gn_line;
     char *line_buf;
     static char *temp_line;
     ssize_t fd_read;
-
+    
+    
     line_buf = (char *)malloc(BUFFER_SIZE + 1);
-    //free(line_buf);
-    
-    
-    
-    while((fd_read = read(fd, line_buf, BUFFER_SIZE)) >0)
-    {
-        //printf("%s\n", line_buf);
+    if (!line_buf)
+        return NULL;
+    //gn_line = NULL;
+    //temp_line = NULL;
+    while ((fd_read = read(fd, line_buf, BUFFER_SIZE)) > 0) {
         line_buf[fd_read] = '\0';
-        if(!temp_line)
-        {
+
+        if (!temp_line) {
             temp_line = ft_strdup(line_buf);
-            if(ft_strchr(temp_line, '\n'))
-            {
-                gn_line = set_line(temp_line);
-                temp_line = ft_strchr(temp_line, '\n');
-            }
-        }
-        else
-        {
+        } else {
             temp_line = ft_strjoin(temp_line, line_buf);
-            //free(line_buf);
+        //     char *temp = ft_strjoin(temp_line, line_buf);
+        //     free(temp_line);
+        //     temp_line = temp;
         }
-        if (line_buf[fd_read - 1] == '\n') {
-            break;
+        if (ft_strchr(temp_line, '\n')) {
+            gn_line = set_line(temp_line);
+            temp_line = ft_strdup(ft_strchr(temp_line, '\n') + 1);
+            return gn_line;
         }
     }
-    if (fd < 0 || BUFFER_SIZE <= 0 || fd_read == -1)
-    {
-        free(line_buf);
-        gn_line = NULL;
-        return (NULL);
-    }
-    printf("temp ---->>>> %s", temp_line);
-    // printf("%s", line_buf);
-    if(!gn_line)
-    {
+    if (fd_read == 0 && temp_line && *temp_line) {
         gn_line = ft_strdup(temp_line);
+        free(temp_line);
+        temp_line = NULL;
+        return gn_line;
     }
-    return (gn_line);
+    free(line_buf);
+    return NULL;
 }
--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 /*   By: ararakel <ararakel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/05 18:01:37 by ararakel          #+#    #+#             */
-/*   Updated: 2025/04/07 19:59:20 by ararakel         ###   ########.fr       */
+/*   Updated: 2025/04/08 18:20:16 by ararakel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,20 @@
 
 int main()
 {
-    char *filename = "text.txt";
+    char *filename = "files/alternate_line_nl_no_nl.txt";
     int fd;
-    //char *line;
+    char *line;
     fd = open(filename, O_RDWR);
     if (fd ==-1)
     {
         printf("%s", "error");
     }
-    // while (get_next_line(fd) != NULL)
-    // {
-    //      line = get_next_line(fd);
-    //     printf("%s", get_next_line(fd));
-    // }
-    // printf("\n%s", get_next_line(fd));
-    printf("final_line ******%s",get_next_line(fd));
+    while ((line = get_next_line(fd)) != NULL)
+    {
+         //line = get_next_line(fd);
+         printf("final_line ******%s",line);
+      //  printf("%s", get_next_line(fd));
+    }
+    //printf("\n%s", get_next_line(fd));
     close(fd);
 }
